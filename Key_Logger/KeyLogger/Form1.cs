aa3b5012eb4186ba237f21a5bde6f5ffcd428ad5
nguyenthithanhsuong <22521268@gm.uit.edu.vn>
2023-11-14 13:08:17 +0700

    Thêm biến cơ bản.

--- Key_Logger/KeyLogger/Form1.cs
+++ Key_Logger/KeyLogger/Form1.cs
@@ -1,9 +1,13 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +16,13 @@ namespace KeyLogger
 {
     public partial class Form1 : Form
     {
+        string file = "Record.txt";
+        string pass = "Password.txt";
+        string decode = "Decode.txt";
+        int s = 0;
+        string k = "";
+        byte[] key = new byte[16];
+        byte[] iv = new byte[16];
         public Form1()
         {
             InitializeComponent();
